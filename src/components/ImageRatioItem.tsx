206878674261cb4686605e97418d88c26d45df4d
hghayebi <53913721+hghayebi@users.noreply.github.com>
2023-09-04 20:36:05 +0330

    Add style to image ratio item

--- src/components/ImageRatioItem.tsx
+++ src/components/ImageRatioItem.tsx
@@ -9,12 +9,26 @@ export default function ImageRatioItem({
 }): React.JSX.Element {
   const { currentRatio } = useAppSelector((state) => state.ratio);
 
-  const divClasses = classNames("flex items-center justify-center");
+  const divClasses = classNames(" ", {
+    "flex items-center justify-center":
+      currentRatio?.vlaue === "1/1" ||
+      currentRatio?.vlaue === "9/16" ||
+      currentRatio?.vlaue === "4/5",
+    "w-96 my-20": currentRatio?.vlaue === "16/9",
+  });
   const imgClasses = classNames(
-    "object-cover h-96",
-    `aspect-[${currentRatio?.vlaue}]`
+    "object-cover",
+    `aspect-[${currentRatio?.vlaue}]`,
+    {
+      "h-96":
+        currentRatio?.vlaue === "1/1" ||
+        currentRatio?.vlaue === "9/16" ||
+        currentRatio?.vlaue === "4/5",
+
+      "w-96": currentRatio?.vlaue === "16/9",
+    }
   );
-  console.log(imgClasses);
+
   return (
     <div className={divClasses}>
       <img className={imgClasses} src={src} alt="album pic" />
