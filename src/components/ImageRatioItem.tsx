17c7666c0814860753021bafaa52e0b7b8df0cbb
hghayebi <53913721+hghayebi@users.noreply.github.com>
2023-09-05 20:13:31 +0330

    style change to ratio item

--- src/components/ImageRatioItem.tsx
+++ src/components/ImageRatioItem.tsx
@@ -20,12 +20,11 @@ export default function ImageRatioItem({
     "object-cover",
     `aspect-[${currentRatio?.vlaue}]`,
     {
-      "h-96":
-        currentRatio?.vlaue === "1/1" ||
-        currentRatio?.vlaue === "9/16" ||
-        currentRatio?.vlaue === "4/5",
+      "h-96 aspect-[1/1]": currentRatio?.vlaue === "1/1",
+      "h-96 aspect-[9/16]": currentRatio?.vlaue === "9/16",
+      "h-96 aspect-[4/5]": currentRatio?.vlaue === "4/5",
 
-      "w-96": currentRatio?.vlaue === "16/9",
+      "w-96 aspect-video": currentRatio?.vlaue === "16/9",
     }
   );
 
